a52dafd13b191cc2a63a77ef51af009287476543
Cheska <cheskatrinidad11@gmail.com>
2025-11-11 16:00:53 +0800

    change to push scripts

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -67,7 +67,7 @@
 </div>
 @endsection
 
-@section('scripts')
+@push('scripts')
     <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
@@ -99,4 +99,4 @@
             });
         });
     </script>
-@endsection
+@endpush
