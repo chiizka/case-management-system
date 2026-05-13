2171e69032636d58d9982663051686acf9b32811
Cheska <cheskatrinidad11@gmail.com>
2026-05-13 11:26:37 +0800

    fix: profile picture not showing on nested routes

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -239,7 +239,7 @@
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">
                                 {{ Auth::user() ? Auth::user()->fname . ' ' . Auth::user()->lname : 'Guest' }}
                             </span>
-                            <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                            <img class="img-profile rounded-circle" src="{{ asset('img/undraw_profile.svg') }}">
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                             <a class="dropdown-item" href="{{ route('profile.index') }}">
